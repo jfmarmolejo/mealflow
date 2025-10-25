a28446d6d01f3f3407b544b8d07a929f1af4e956
jfmarmolejo <jfmarmolejo@hotmail.com>
2025-10-25 12:01:26 -0500

    backend preview

---
+++ server.js
@@ -0,0 +1,25 @@
+'use strict'
+const express = require('express')
+const cors = require('cors')
+const app = express()
+// app.use(express.json())
+
+app.use(cors())
+const lambdaCheck = require('./backend/lambdas/src/checker')
+
+app.get('/', (req, res)=>{
+    res.send('Home API')
+})
+
+app.get('/check', async (req, res)=>{
+    try{
+        const lambdaResponse = await lambdaCheck.handler('Greetings.')
+        res.send(lambdaResponse.payload) 
+    } catch(err){
+        res.send(`Lambda error: ${err.message}`) 
+    }
+})
+
+app.listen(3001, ()=>{
+    console.log(`Server listen at http://localhost:3001`)
+})
